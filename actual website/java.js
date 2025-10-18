22bea55653ce179b378301ef464b3e4ef8dd13a2
M0bile132024 <22chimaobisoronnadi@st-ambrosecollege.org.uk>
2025-10-18 16:50:31 +0100

    THE MOBILE WEBSITE VERSION 1.4:The Jarvis Update
    Enhance mobile website: Add interactive buttons and JavaScript functionality; update HTML structure and styles across multiple pages.

---
+++ actual website/java.js
@@ -0,0 +1,30 @@
+// This is a basic JavaScript template to get started
+
+// Function to display a greeting message
+function displayGreeting() {
+    const greeting = "Welcome to the Mobile Website!";
+    console.log(greeting);
+    alert(greeting);
+}
+
+// Call the function when the page loads
+window.onload = function() {
+    displayGreeting();
+};
+// Additional functionality can be added here
+// For example, handling button clicks or form submissions
+function handleButtonClick() {
+    console.log("Button was clicked!");
+    alert("You clicked the button!Now Jarvis knows your name (o≧▽≦)o.");
+}
+// Example of adding an event listener to a button
+document.getElementById("mybuton").addEventListener("click", handleButtonClick);
+//Varibles
+var userName = prompt("Please enter your name:");
+console.log("User's name is: " + userName);
+// End of JavaScript code
+
+// THE MOBILE WEBSITE VERSION 1.4:The Jarvis Update
+// Author: M0bile132022
+// Date of last test: 18/10/25
+
